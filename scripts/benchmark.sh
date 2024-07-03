23144391521394fc7c9b9586eadd8db66e1a00f6
Divan Visagie <me@divanv.com>
2024-07-03 03:13:28 +0200

    Update benchmark

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Benchmarking cache hit time:"
-hyperfine --warmup 3 "csep 'file containing typescript'"
+csep cache -c
+echo "Benchmarking cache hit time for fastembed"
+hyperfine --warmup 3 "csep 'file containing typescript' --client fastembed"
 
+csep cache -c
+echo "Benchmarking cache hit time for ollama"
+hyperfine --warmup 3 "csep 'file containing typescript' --client ollama"
 
 echo "Benchmarking cache build time for fastembed:"
 hyperfine --warmup 3 "csep cache -c; csep --client fastembed 'file containing typescript'"
